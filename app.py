e7a10e3bbdc7b1be0ad6a813dba83fe28dcb9d7e
Carlos González Heredia <33523101+carlosgh89@users.noreply.github.com>
2024-10-01 10:59:15 +0200

    lee luista
    asdasd

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sys
 import time
 import pyautogui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit
+import pandas as pd
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QFileDialog
 
 class MouseTrackerApp(QMainWindow):
     cantidad = 0
@@ -9,15 +10,22 @@ class MouseTrackerApp(QMainWindow):
         super().__init__()
         self.initUI()
         self.positions = []
+        self.excel_file = ""
 
     def initUI(self):
-        self.setWindowTitle('Mouse Tracker')
-        self.setGeometry(100, 100, 500, 200)  # Increase width by 200 pixels
+        self.setWindowTitle('ENtradas Auto')
+        self.setGeometry(100, 100, 500, 300)  # Increase width by 200 pixels and height by 100 pixels
 
-        self.button2 = QPushButton('Run', self)
+        self.button2 = QPushButton('Añade un valor', self)
         self.button2.clicked.connect(self.trabaja)
 
         self.text_input = QLineEdit(self)
+
+        self.file_button = QPushButton('Selecciona archivo Excel', self)
+        self.file_button.clicked.connect(self.select_file)
+
+        self.process_button = QPushButton('Process Excel File', self)
+        self.process_button.clicked.connect(self.process_excel_file)
 
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.button2)
@@ -25,14 +33,15 @@ class MouseTrackerApp(QMainWindow):
 
         layout = QVBoxLayout()
         layout.addLayout(h_layout)
+        layout.addWidget(self.file_button)
+        layout.addWidget(self.process_button)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
-    #definimos una funcion que acepta un int y lo imprime  
+
     def suma(self, cantidad):
-        self.cantidad = str(cantidad)
-        cantidad=str(100)
+        cantidad = str(cantidad)
         pyautogui.moveTo(114, 497, duration=2)
         pyautogui.click()
         pyautogui.moveTo(1583, 293, duration=2)
@@ -50,22 +59,57 @@ class MouseTrackerApp(QMainWindow):
         pyautogui.moveTo(951, 862, duration=2)
         pyautogui.click()
 
-
     def trabaja(self):
         # abre el navegador y entra a la pagina https://coinmarketcap.com/portfolio-tracker/
         pyautogui.hotkey('win', 'r')
         time.sleep(1)
-        pyautogui.typewrite('chrome')
+        pyautogui.typewrite('cmd')
         pyautogui.press('enter')
-        time.sleep(2)
-        pyautogui.typewrite('https://coinmarketcap.com/portfolio-tracker/')
+        time.sleep(5)
+        pyautogui.typewrite('start chrome /MAX https://coinmarketcap.com/portfolio-tracker/')
         pyautogui.press('enter')
+        time.sleep(5)
 
         self.suma(self.text_input.text())
 
+    def trabajaGrupo(self, listaEntradas):
+        # abre el navegador y entra a la pagina https://coinmarketcap.com/portfolio-tracker/
+        pyautogui.hotkey('win', 'r')
+        time.sleep(1)
+        pyautogui.typewrite('cmd')
+        pyautogui.press('enter')
+        time.sleep(5)
+        pyautogui.typewrite('start chrome /MAX https://coinmarketcap.com/portfolio-tracker/')
+        pyautogui.press('enter')
+        time.sleep(5)
+        for i in listaEntradas:
+            self.suma(i)
+            time.sleep(2)
 
+    def select_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select Excel File", "", "Excel Files (*.xlsx *.xls)", options=options)
+        if file_name:
+            self.excel_file = file_name
+            print(f"Selected file: {self.excel_file}")
 
-        
+    def process_excel_file(self):
+        if self.excel_file:
+            lista_numeros = self.obtener_lista_numeros(self.excel_file)
+            print(f"Numbers from Excel: {lista_numeros}")
+            self.trabajaGrupo(lista_numeros)
+        else:
+            print("No file selected")
+
+        #vamos a iterar sobre la lista de numeros 
+
+    def obtener_lista_numeros(self, archivo_excel):
+        # Lee el archivo de Excel
+        df = pd.read_excel(archivo_excel, usecols="A", skiprows=1, nrows=9)
+        # Obtiene la lista de números desde el rango A2:A10
+        lista_numeros = df.iloc[:, 0].tolist()
+        return lista_numeros
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
